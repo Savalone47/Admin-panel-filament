b5b66ba34c84803caa1aab7e221688344db1799d
savalone47 <15yn450@esisalama.org>
2023-03-01 14:25:54 +0200

    add searchable and sortable id

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,10 +10,11 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run(): void{
 
+        $this->call(RolesPermissionSeeder::class);
+
+        // \App\Models\User::factory(10)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
